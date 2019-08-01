bf88e9d91dbf2fbee8d2b95581cc274ea0b7d479
Daniel Torres <danieljtorres94@gmail.com>
2019-08-01 19:26:24 +0000

    .

---
+++ app/Middleware/XFrameOpt.js
@@ -0,0 +1,19 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+class XFrameOpt {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ request }, next) {
+    // call next to advance the request
+    response.header('X-FRAME-OPTIONS', 'ALLOW-FROM ' + request.url())
+    await next()
+  }
+}
+
+module.exports = XFrameOpt
